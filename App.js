3b9e7d6af050e463aacdd86d638e9c3e04f3e8ab
Srinilaya Pasupureddi <srinilayap@gmail.com>
2025-06-20 12:59:19 +0530

    Day-2

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 const heading=React.createElement('h1',{id:"heading"}, 'Hello, World from React!');
 const root=ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
 
-const heading2=React.createElement("div",{class:"parent"},
-              React.createElement("div",{class:"child"},
-              [
-              React.createElement("h1",{id:"heading2"}, 'Hello, World from h1!'),
-              React.createElement("h2",{id:"heading2"}, 'Hello, World from h2!')
-              ])
+const heading2 = React.createElement("div", { className: "parent" },
+  React.createElement("div", { className: "child" }, [
+    React.createElement("h1", { id: "heading2-h1", key: "h1" }, 'Hell, World from h1!'),
+    React.createElement("h2", { id: "heading3", key: "h2" }, 'Hello, World from h2 and Nilaya!')
+  ])
 );
+
 const  root2=ReactDOM.createRoot(document.getElementById('root2'));
 root2.render(heading2);
